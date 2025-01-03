a44bae00125581401ab3164a62e6a65662fd80a3
Jason Blackburn <blackburnjason1994@gmail.com>
2025-01-03 18:58:13 +0000

    comments out currently unsused imports

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ import { useGSAP } from "@gsap/react";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import Image from "next/image";
 import hero from "../../public/logo.svg";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function Hero() {
   gsap.registerPlugin(ScrollTrigger, useGSAP);
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Gallery from "./components/Gallery";
+// import Gallery from "./components/Gallery";
 import Hero from "./components/Hero";
 
 export default function Home() {
